f8caae45c558a247585d3e5b34e895e09204155e
MAHENDRA TEKUDE <129305588+MahiTek007@users.noreply.github.com>
2024-03-20 13:22:56 +0000

    searching

--- Array/Searching/Binary Search/P1.java
+++ Array/Searching/Binary Search/P1.java
@@ -7,14 +7,41 @@ public class P1 {
     public static void main(String[] args) {
         int nums []= {1,2,3,4,5,6,7,8,9};
         System.out.println("Enter Your Target");
-        @SuppressWarnings("resource")
         int terget = new Scanner(System.in).nextInt();
-        int index =binarySearch(nums, terget);
-        if (index ==-1) {
-            System.out.println("Your Targeted Element is Not Present Inside array...");
-        }else{
-            System.out.println("Your Targeted Element is"+nums[index]+" Its index is: "+index);
+
+        System.out.println("Enter Your Choice");
+        int choice = new Scanner(System.in).nextInt();
+        
+        if (choice==1) {
+            int index =binarySearch(nums, terget);
+            if (index ==-1) {
+                System.out.println("Your Targeted Element is Not Present Inside array...");
+            }else{
+                System.out.println("Your Targeted Element is"+nums[index]+" Its index is: "+index);
+            }
+        }else if (choice==2) {
+            int index2 =binarySearch(nums, terget,0,nums.length-1);
+            if (index2 ==-1) {
+                System.out.println("Your Targeted Element is Not Present Inside array...");
+            }else{
+                System.out.println("Your Targeted Element is"+nums[index2]+" Its index is: "+index2);
+            }
         }
+    }
+
+    private static int binarySearch(int[] nums, int terget, int left, int right) {
+        Arrays.sort(nums);
+        if (left<=right) {
+            int mid=(left+right)/2;
+           if (terget==nums[mid]) {
+             return mid;
+           } else if (nums[mid]<right) {
+              return binarySearch(nums, terget,mid-1, right);
+           }else{
+              return binarySearch(nums, terget, mid+1, right);
+           }
+        }
+        return -1;
     }
 
     private static int binarySearch(int[] nums, int terget) {
